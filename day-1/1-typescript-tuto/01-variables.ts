5a517173ed70969e0d6c364bc99dffb38b5a0c88
Ruthran RAGHAVAN <hnai@Ruthrans-MacBook-Pro.local>
2026-06-22 13:53:35 +0530

    day-1 tp first activity

---
+++ day-1/1-typescript-tuto/01-variables.ts
@@ -0,0 +1,8 @@
+// what is a variable?
+// A variable is a named storage that can hold a value.
+// In programming, variables are used to store data that
+// can be manipulated and accessed throughout the code.
+// In TypeScript, variables can be declared using
+// the `let`, `const`, or `var` keywords, each
+// with different scoping rules and mutability.
+
